bccf416451f9efd14eddd1c0d72532bde84a8c1a
Mehul Agarwal <agarwalmehul005@gmail.com>
2024-03-24 13:26:43 +0000

    Updated theme.js

--- src/theme.js
+++ src/theme.js
@@ -3,10 +3,10 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#f50057', // Change this to your primary color
+      main: '#0000FF', // Change this to your primary color
     },
     secondary: {
-      main: '#ba000d', // Change this to your secondary color
+      main: '#0000FF', // Change this to your secondary color
     },
   },
 });
